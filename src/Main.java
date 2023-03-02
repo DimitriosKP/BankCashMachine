9d8cacbd8d1543bee86126b182f748cb2cbb6ea2
DimitriosKP <dimitrios.peftitsis@gmail.com>
2023-03-02 18:26:08 +0200

    fix 2/3/2023

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 import java.sql.*;
-import gui.*;
 import static api.Users.login_panel;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        //login_panel();
-        Login.showLoginForm();
+        login_panel();
     }
 }
